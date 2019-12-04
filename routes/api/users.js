e3fb0d20a73fad14f7fc0e865a42e8e9bc397263
Forrest Smith <forrest216@gmail.com>
2019-12-04 07:56:47 -0800

    fix to backend for JWT implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const usersController = require('../controllers/users');
+const User = require('../../models/user');
+const usersController = require('../../controllers/users');
 
-router.get('/', usersController.getUsers);
+router.post('/signup', usersController.signup);
+router.post('/login', usersController.login);
 
 module.exports = router;
