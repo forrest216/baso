531b35366845b4a7fe16136b380aa91bb255dde5
Forrest Smith <forrest216@gmail.com>
2019-12-04 08:43:24 -0800

    minor api edit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/user');
 const usersController = require('../../controllers/users');
 
 router.post('/signup', usersController.signup);
