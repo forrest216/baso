fa05c61846e1d5dc541e3bfcccae241d65863d66
Forrest Smith <forrest216@gmail.com>
2019-12-24 23:48:26 -0800

    fix social media links to open in new tab

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import fb from '../../images/fb.png';
 const Footer = (props) => {
    return ( 
       <div>
-         <a href="https://www.instagram.com/basofibonacci/?hl=en"><img src="https://en.instagram-brand.com/wp-content/themes/ig-branding/prj-ig-branding/assets/images/ig-logo-black.svg" alt="insta"/></a>
-         <a href="https://www.facebook.com/baso.fibonacci"><img src={fb} alt="fb" height="37" width="37"/></a>
+         <a href="https://www.instagram.com/basofibonacci/?hl=en" target="_blank"><img src="https://en.instagram-brand.com/wp-content/themes/ig-branding/prj-ig-branding/assets/images/ig-logo-black.svg" alt="insta"/></a>
+         <a href="https://www.facebook.com/baso.fibonacci" target="_blank"><img src={fb} alt="fb" height="37" width="37"/></a>
       </div>
     );
 }
